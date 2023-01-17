c0dc16ac89756a2216a87573c19461b15234637a
emmanuelojiji <ojijimanuel@hotmail.com>
2023-01-17 18:28:22 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.scss";
+import Header from "./Components/Header";
+import Question1 from "./Components/Question1";
+import Question2 from "./Components/Question2";
 
 function App() {
+  const [name, setName] = useState("");
+
+  const [view, setView] = useState(1)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {view === 1 && (
+        <Question1 enteringName={(e) => setName(e.target.value)} />
+      )}
+
+{view === 2 && (
+        <Question2 enteringName={(e) => setName(e.target.value)} />
+      )}
+      {name}
     </div>
   );
 }
